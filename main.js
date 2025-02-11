f90c5b9ef7dc700277d89e401d0b221c617e4e1d
Bobb-93 <borisiakimov@gmail.com>
2025-02-11 15:32:39 +0200

    category and difficulty select (visible/not visible)

--- main.js
+++ main.js
@@ -9,14 +9,37 @@ let quizCategory = null;
 let dom = {
     beginButton: document.getElementById("begin-button"),
     categorySelect: document.getElementById("category-select"),
+    difficultyLabel: document.getElementById("difficulty-label"),
     difficultySelect: document.getElementById("difficulty-select")
 };
 
-let selectedCategory = dom.categorySelect.selectedOptions;
-console.log(selectedCategory);
+dom.categorySelect.addEventListener("change", function () {
+    if(dom.categorySelect.value){
+        dom.difficultyLabel.style.visibility = "visible";
+        dom.difficultySelect.style.visibility = "visible";
+    } else {
+        //just in case
+        dom.difficultyLabel.style.visibility = "visible";
+        dom.difficultySelect.style.visibility = "visible";
+        dom.beginButton.style.visibility = "hidden";
 
+    }
+});
+
+dom.difficultySelect.addEventListener("change", function () {
+    if(dom.difficultySelect.value){
+        dom.beginButton.style.visibility = "visible";
+    } else {
+        //just in case
+        dom.beginButton.style.visibility = "hidden";
+    }
+})
 
 dom.beginButton.addEventListener("click", function () {
-    location.assign("./quiz.html");  
+    const category = document.getElementById("category-select").value;
+    const difficulty = document.getElementById("difficulty-select").value;
+    window.location.href = `quiz.html?category=${category}&difficulty=${difficulty}`;
+
+    // location.assign("./quiz.html");  
 });
 
